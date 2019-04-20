87458eb4ebc436852f53e065d912da689e676772
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-04-20 15:33:40 +0900

    とりあえず書いた

--- Dozap/src/controller.cpp
+++ Dozap/src/controller.cpp
@@ -1,5 +1,24 @@
 #include<ros/ros.h>
 #include<sensor_msgs/Joy.h>
-#include<Dozap/msg/button.h>
+#include"Dozap/msg/button.h"
 void joy_callback(const sensor_msgs::Joy& joy_msgs){
-    
+    data.move = joy_msg.axes[3];
+    data.rotation_right = joy_msg[9];
+    data.rotation_left = joy_msg[8];
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "controller");
+    ros::NodeHandle n;
+    ros::Publisher controller_pub = n.advertise<geometry_msgs::button>("controller_info", 1000);
+    ros::Subscriber joy_sub = n.advertise("joy", 10, joy_callback);
+    ros::Rate loop_rate(10);
+
+    while(ros::ok()){
+        controller_pub.publish(data);
+        ros::SpinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
+
