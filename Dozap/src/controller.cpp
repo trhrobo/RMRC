dda4d7fb71b2c298b05f54c01fd3aaca1470220d
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-04-20 16:00:43 +0900

    変更

--- Dozap/src/controller.cpp
+++ Dozap/src/controller.cpp
@@ -10,11 +10,12 @@ void joy_callback(const sensor_msgs::Joy& joy_msgs){
 int main(int argc, char **argv){
     ros::init(argc, argv, "controller");
     ros::NodeHandle n;
-    ros::Publisher controller_pub = n.advertise<geometry_msgs::button>("controller_info", 1000);
+    ros::Publisher controller_pub = n.advertise<Dozap::button>("controller_info", 1000);
     ros::Subscriber joy_sub = n.advertise("joy", 10, joy_callback);
     ros::Rate loop_rate(10);
 
     while(ros::ok()){
+        Dozap::button data;
         controller_pub.publish(data);
         ros::SpinOnce();
         loop_rate.sleep();
--- Dozap/src/plan.cpp
+++ Dozap/src/plan.cpp
@@ -1,5 +1,10 @@
 #include<ros/ros.h>
+#include<Dozap/msg/Plan.h>
 
+void controller_callback(const Dozap::Plan & )
 int main(int argc, char **argv){
   ros::init(argc, argv, "plan");
   ros::NodeHandle n;
+  ros::Publisher plan_pub = n.advertise<Dozap::Plan>("Plan_info", 1000);
+  ros::Subscriber controller_sub = n.advertise("controller_info", 1000, controller_callback);
+
