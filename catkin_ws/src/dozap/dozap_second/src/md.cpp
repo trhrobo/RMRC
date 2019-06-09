d96c9628bc178ecfed4d3f79a758d7afc378d6d0
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-06-09 01:07:12 +0000

    motor success

--- catkin_ws/src/dozap/dozap_second/src/md.cpp
+++ catkin_ws/src/dozap/dozap_second/src/md.cpp
@@ -9,8 +9,8 @@ constexpr int LEFT_MOTOR_IN1 = 14;
 constexpr int LEFT_MOTOR_IN2 = 15;
 constexpr int LEFT_MOTOR_PWM = 18;
 constexpr int MOTOR_FREQ = 125;
-constexpr int RIGHT_DUTY = 255;
-constexpr int LEFT_DUTY = 255;
+constexpr int RIGHT_DUTY = 100;
+constexpr int LEFT_DUTY = 100;
 constexpr int RIGHT_ROTATION_1 = 100;
 constexpr int RIGHT_ROTATION_2 = 80;
 constexpr int LEFT_ROTATION_1 = 100;
@@ -34,7 +34,7 @@ void main_callback(const dozap_second::Main &main_msg){
     if(main_msg.motor_right < 0){motor_right_ccw();}
     if(main_msg.motor_right > 0){motor_left_cw();}
     if(main_msg.motor_right < 0){motor_left_ccw();}
-    //if(main_msg.motor_right == 0){motor_stop();}
+    if(main_msg.motor_right == 0 || main_msg.motor_left == 0){motor_stop();}
     if(main_msg.rotation_a_right > 0){motor_right_cw();}
     if(main_msg.rotation_b_right > 0){motor_right_cw();}
     if(main_msg.rotation_a_right < 0){motor_right_ccw();}
@@ -62,14 +62,14 @@ int main(int argc, char **argv){
     ros::NodeHandle n;
     ros::Subscriber md_sub = n.subscribe("pwm_info", 10, main_callback);
     pi = pigpio_start(0, 0);
-    set_mode(pi, RIGHT_MOTOR_IN1, PI_OUTPUT);
-    set_mode(pi, RIGHT_MOTOR_IN2, PI_OUTPUT);
-    set_mode(pi, RIGHT_MOTOR_PWM, PI_OUTPUT);
-    set_mode(pi, LEFT_MOTOR_IN1, PI_OUTPUT);
-    set_mode(pi, LEFT_MOTOR_IN2, PI_OUTPUT);
-    set_mode(pi, LEFT_MOTOR_PWM, PI_OUTPUT);
-    set_PWM_frequency(pi, RIGHT_MOTOR_PWM, MOTOR_FREQ);
-    set_PWM_frequency(pi, LEFT_MOTOR_PWM, MOTOR_FREQ);
+    set_mode(pi, RIGHT_MOTOR_IN1, 1);
+    set_mode(pi, RIGHT_MOTOR_IN2, 1);
+    set_mode(pi, RIGHT_MOTOR_PWM, 1);
+    set_mode(pi, LEFT_MOTOR_IN1, 1);
+    set_mode(pi, LEFT_MOTOR_IN2, 1);
+    set_mode(pi, LEFT_MOTOR_PWM, 1);
+//    set_PWM_frequency(pi, RIGHT_MOTOR_PWM, MOTOR_FREQ);
+//    set_PWM_frequency(pi, LEFT_MOTOR_PWM, MOTOR_FREQ);
 
 
     ros::Rate loop_rate(10);
@@ -82,20 +82,20 @@ int main(int argc, char **argv){
 }
 
 void motor_right_cw(void){
-     gpio_write(pi, RIGHT_MOTOR_IN1, 1);
-     gpio_write(pi, RIGHT_MOTOR_IN1, 0);
      set_PWM_dutycycle(pi, RIGHT_MOTOR_PWM, RIGHT_DUTY);
+     gpio_write(pi, RIGHT_MOTOR_IN1, 1);
+     gpio_write(pi, RIGHT_MOTOR_IN2, 0);
      ROS_INFO("cw");
 }
 void motor_right_ccw(void){
+     set_PWM_dutycycle(pi, RIGHT_MOTOR_PWM, RIGHT_DUTY);
      gpio_write(pi, RIGHT_MOTOR_IN1, 0);
      gpio_write(pi, RIGHT_MOTOR_IN2, 1);
-     set_PWM_dutycycle(pi, RIGHT_MOTOR_PWM, RIGHT_DUTY);
      ROS_INFO("ccw");
 }
 void motor_left_cw(void){
      gpio_write(pi, LEFT_MOTOR_IN1, 1);
-     gpio_write(pi, LEFT_MOTOR_IN1, 0);
+     gpio_write(pi, LEFT_MOTOR_IN2, 0);
      set_PWM_dutycycle(pi, LEFT_MOTOR_PWM, LEFT_DUTY);
      ROS_INFO("cw");
 }
@@ -146,7 +146,7 @@ void motor_rotation_back_left(void){
      set_PWM_dutycycle(pi, LEFT_MOTOR_PWM, LEFT_ROTATION_1);
      ROS_INFO("back_left");
 }
-   
+*/   
 void motor_stop(void){
      gpio_write(pi, LEFT_MOTOR_IN1, 0);
      gpio_write(pi, LEFT_MOTOR_IN2, 0);
@@ -157,4 +157,4 @@ void motor_stop(void){
      ROS_INFO("stop");
 }
 
-*/	
+	
