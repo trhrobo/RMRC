1b0079e28456d2b2f8946090bc25831f30a402a1
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-06-20 14:31:18 +0000

    ライブラリ用のブランチ切った

---
+++ catkin_ws/src/rmrc_utility/include/md.hpp
@@ -0,0 +1,10 @@
+namespace ros{
+class MD{
+public: 
+	void MD(int pin_A, int pin_B, int pin_pwm);
+	void OUTPUT(int check, int pwm);
+private:
+	int user_A, user_B, int user_pwm;
+};
+}
+
---
+++ catkin_ws/src/rmrc_utility/src/md.cpp
@@ -0,0 +1,22 @@
+#include<pigpiod_if2.h>
+
+using ros::MD;
+
+MD::MD(int pin_A, int pin_B, int pwm_pin){
+	user_A = pin_A;
+	user_B = pin_B;
+	user_pwm = pwm_pin;
+}
+
+MD::OUTPUT(int check, int pwm);
+	if(check){
+		gpio_write(pi, user_A, 1);
+		gpio_write(pi, user_B, 0);
+		set_PWM_dutycycle(pi, pwm_pin, pwm);
+	}else{
+		
+		gpio_write(pi, user_A, 0);
+		gpio_write(pi, user_B, 1);
+		set_PWM_dutycycle(pi, pwm_pin, pwm);
+	}
+}
