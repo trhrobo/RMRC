c3fabd2147af47bf9759931c77e40946f96075b8
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-06-20 14:45:27 +0000

    test

--- catkin_ws/src/rmrc_utility/src/md.cpp
+++ catkin_ws/src/rmrc_utility/src/md.cpp
@@ -8,15 +8,17 @@ MD::MD(int pin_A, int pin_B, int pwm_pin){
 	user_pwm = pwm_pin;
 }
 
-MD::OUTPUT(int check, int pwm);
+MD::OUTPUT(int check, int pwm){
+//check == 1 front
+//check == 0 back
 	if(check){
 		gpio_write(pi, user_A, 1);
 		gpio_write(pi, user_B, 0);
-		set_PWM_dutycycle(pi, pwm_pin, pwm);
 	}else{
 		
 		gpio_write(pi, user_A, 0);
 		gpio_write(pi, user_B, 1);
-		set_PWM_dutycycle(pi, pwm_pin, pwm);
 	}
+	set_PWM_dutycycle(pi, pwm_pin, pwm);
+	
 }
