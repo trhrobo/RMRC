5390f96371e9e5cf6e2c61fa672c0ac811d8de36
TsuruharaKota <m1830@s.akashi.ac.jp>
2020-01-02 11:30:23 +0900

    change

--- dynamixel_servo/src/set_servo_pos.cpp
+++ dynamixel_servo/src/set_servo_pos.cpp
@@ -14,9 +14,13 @@ using std::cout;
 using std::endl;
 
 ros::Publisher arm_pub;
-int plus_flag = 2;
-int prev_plus_flag = 2;
-double prev_value = 0;
+// int flag_plus = 2;
+// int prev_flag_plus = 2;
+// double prev_value = 0;
+// 初期値が2であるのははじめは回転しないようにするため
+// 現在の角度を読み取ってその角度を加減すると初期値で回転しなくても良くなる
+vector<int> flag_plus{2, 2, 2, 2};
+vector<int> prev_flag_plus { 2, 2, 2, 2 }
 int call = 0;
 double check = 0;
 vector<double> goal_angle{0, 0, 0, 0};
@@ -27,18 +31,26 @@ bool flag_all = false;
 std_msgs::String joint1_name;
 std_msgs::Float64 joint1_pos;
 
-enum servoStatus { FORWARD; FORWARD_ALL; REVERSE; REVERSE_ALL; DESIGNATION; }
+enum servoStatus {
+  FORWARD; FORWARD_ALL; REVERSE; REVERSE_ALL; DESIGNATION; BRAKE;
+}
 
+// flag_plus == 1だと正転
+// flag_plus == 3だと逆転
 void joyCallback(const sensor_msgs::Joy &controller) {
-  if (controller.axes[5] < 0) {
-    plus_flag = 1;
-    check = controller.axes[5];
+  if (controller.axes[5] < 0 or controller.axes[1] < 0 or
+      controller.axes[2] < 0 or controller.axes[3] < 0) {
+    flag_plus = 1;
   }
-  if (controller.buttons[5]) {
-    plus_flag = 3;
+  if (controller.buttons[5] or controller.buttons[1] or controller.buttons[2] or
+      controller.buttons[3]) {
+    flag_plus = 3;
   }
-  check = controller.axes[5];
-  call = controller.buttons[5];
+  controller.buttons[4] ? flag_all = true : flag_all = false;
+
+  if (controller.axes[5] == 0 and controller.buttons[5] == 0) {
+    flag_brake = true;
+  }
 }
 
 void monitorJointState_callback(const sensor_msgs::JointState &jointstate) {
@@ -62,12 +74,10 @@ void checkJoint() {
 void forward() {
   for (int i < 0; i < flag_joint_check.size(); ++i) {
     if (flag_joint_check[i]) {
-      if (plus_flag == 1) {
-        jtp0.points[0].positions[i] += 2;
-      }
-      if (check > 0 && call == 0) {
+      jtp0.points[0].positions[i] += 2;
+      /*if (check > 0 && call == 0) {
         jtp0.points[0].positions[i] = prev_value;
-      }
+      }*/
     }
   }
 }
@@ -75,39 +85,39 @@ void forward() {
 void forwardAll() {
   for (int i < 0; i < flag_joint_check.size(); ++i) {
     if (flag_joint_check[i]) {
-      if (plus_flag == 1) {
-        jtp0.points[0].positions[i] += 2;
-      }
-      if (check > 0 && call == 0) {
+      jtp0.points[0].positions[i] += 2;
+      /*if (check > 0 && call == 0) {
         jtp0.points[0].positions[i] = prev_value;
-      }
+      }*/
     }
   }
 }
 
 void reverse() {
   for (int i < 0; i < flag_joint_check.size(); ++i) {
-    if (flag_joint_check[i]) {
-      if (plus_flag == 3) {
-        jtp0.points[0].positions[i] -= 2;
-      }
-      if (check > 0 && call == 0) {
-        jtp0.points[0].positions[i] = prev_value;
-      }
-    }
+    if (flag_joint_check[i])
+      jtp0.points[0].positions[i] -= 2;
+    /*if (check > 0 && call == 0) {
+      jtp0.points[0].positions[i] = prev_value;
+    }*/
   }
+}
 }
 
 void reverseAll() {
   for (int i < 0; i < flag_joint_check.size(); ++i) {
     if (flag_joint_check[i]) {
-      if (plus_flag == 1) {
-        jtp0.points[0].positions[i] += 2;
-      }
-      if (check > 0 && call == 0) {
+      jtp0.points[0].positions[i] += 2;
+      /*if (check > 0 && call == 0) {
         jtp0.points[0].positions[i] = prev_value;
-      }
+      }*/
     }
+  }
+}
+
+void brake() {
+  for (int i < 0; i < flag_joint_check.size(); ++i) {
+    jtp0.points[0].positions[i] = prev_value;
   }
 }
 
@@ -123,9 +133,14 @@ void angleCal() {
   if (flag _designation) {
     now_status = DESIGNATION;
   } else if (flag_all) {
-    flag_rotate == 1 ? now_status = FORWARD_ALL : now_status = REVERSE_ALL;
+    flag_plus == 1 ? now_status = FORWARD_ALL : flag_plus == 3
+                                                    ? now_status = REVERSE_ALL
+                                                    : now_status = BRAKE;
+  } else if (flag_brake) {
+    now_status = BRAKE;
   } else {
-    flag_rotate == 1 ? now_status = FORWARD : now_status = REVERSE;
+    flag_plus == 1 ? now_status = FORWARD
+                   : flag_plus == 3 ? now_status = REVERSE : now_status = BRAKE;
   }
 
   switch (status) {
@@ -143,6 +158,9 @@ void angleCal() {
     break;
   case DESIGNATION:
     designetaion();
+    break;
+  case BRAKE:
+    brake();
     break;
   }
 }
@@ -172,9 +190,9 @@ int main(int argc, char **argv) {
     ros::spinOnce(); // コールバック関数を呼ぶ
     if (joint1_pos.data + 0.5 > prev_value &&
         joint1_pos.data - 0.5 < prev_value) {
-      if (plus_flag == 1) {
+      if (flag_plus == 1) {
         jtp0.points[0].positions[0] += 2;
-      } else if (plus_flag == 3) {
+      } else if (flag_plus == 3) {
         jtp0.points[0].positions[0] -= 2;
       }
       if (check > 0 && call == 0) {
@@ -188,9 +206,9 @@ int main(int argc, char **argv) {
     jtp0.points[0].positions[0] = abs(jtp0.points[0].positions[0]);
     arm_pub.publish(jtp0);
     prev_value = jtp0.points[0].positions[0];
-    prev_plus_flag = plus_flag;
+    prev_flag_plus = plus_flag;
     loop_rate.sleep();
-    cout << "plus flag = " << plus_flag << endl;
+    cout << "plus flag = " << flag_plus << endl;
     ROS_INFO("Joint1(%s)= %f", jtp0.joint_names[0].c_str(),
              jtp0.points[0].positions[0]);
   }
