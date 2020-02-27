4ce0209fcbe7a6156e011420f2c4655688acf7c9
TsuruharaKota <m1830@s.akashi.ac.jp>
2020-02-28 02:59:33 +0900

    change

--- comprehensive/src/stm_serial.cpp
+++ comprehensive/src/stm_serial.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
 #include<pigpiod_if2.h>
 #include<ros/ros.h>
-#include<std_msgs/Float64MultiArray.h>
+#include<std_msgs/Float32MultiArray.h>
 #include<thread>
 
 using std::thread;
 using std::cout;
 using std::endl;
 
+#define HEAD_BYTE 0xFF
+#define STX 0x02
+
 const char *port = "/dev/ttyAMA0";
 int baudrate = 115200;
 int pi = pigpio_start(0, 0);
 int serial_handle{};
 //send
-uint8_t dataByte[2];
-uint8_t checksum_send;
-uint16_t send_data = 0;
+uint16_t send_data[2]{};
 //receive
 uint8_t got_data{};
 uint8_t byte_now{};
 uint8_t byte[2]{};
-uint16_t result{};
+uint32_t receive_result[5]{};
 
 typedef struct{
   uint16_t gyro_data;
@@ -28,60 +29,77 @@ typedef struct{
 }ReceiveFormat;
 
 void sendSerial(){
-  checksum_send = 0;
-  for(int k = 0; k < 2; ++k){
-    if(send_data[k] > 1000)send_data[k] = 0;
-    dataByte[0] = (send_data[k] >> 8) & 0xFF;
-    dataByte[1] = (send_data[k] >> 0) & 0xFF;
-    //send head byte
-    serial_write_byte(pi, serial_handle, 0x02);
-    for (int i = 0; i < 2; ++i) {
-      if ((dataByte[i] == 0x7D) || (dataByte[i] == 0x02)) {
-        serial_write_byte(pi, serial_handle, 0x7D);
-        checksum_send += 0x7D;
-        serial_write_byte(pi, serial_handle, dataByte[i] ^ 0x20);
-        checksum_send += dataByte[i] ^ 0x20;
-      } else {
-        serial_write_byte(pi, serial_handle, dataByte[i]);
-        checksum_send += dataByte[i];
-      }
+  uint8_t checksum_send = 0;
+  for(int i = 0; i < 2; ++i){
+    unsigned char data_h_h[i] = (send_data[i] >> 32) & 0xFF;
+    unsigned char data_h_l[i]= (send_data[i] >> 24) & 0xFF;
+    unsigned char data_l_l[i] = (send_data[i] >> 16) & 0xFF;
+    unsigned char data_l_l[i] = (send_data[i] >> 8) & 0xFF;
+  }
+  unsigned char sendFormat[2][5] = {
+    {0, data_h_h[0], data_h_l[0], data_l_h[0], data_l_l[0]},
+    {1, data_h_h[1], data_h_l[1], data_l_h[1], data_l_l[1]},
+  }
+  //send head byte
+  serial_write_byte(pi, serial_handle, HEAD_BYTE);
+  check_sum_send += HEAD_BYTE;
+  serial_write_byte(pi, serial_handle, STX);
+  check_sum_send += STX;
+  for(int i = 0; i < 2; ++i){
+    for(int k = 0; k < 5; ++k){
+      serial_write_byte(pi, serial_handle, sendFormat[i][k]);
+      check_sum_send += sendFormat[i][k];
     }
   }
+  //send checksum
   serial_write_byte(pi, serial_handle, checksum_send);
 }
 void receiveSerial(){
-  uint8_t checksum{};
+  uint8_t checksum_receive{};
+  uint8_t receive_data[5];
+  unsigned char receiveFormat[5][5] = {
+    {0, data_h_h[i], data_h_l[i], data_l_h[i], data_l_l[i]},
+    {1, data_h_h[i], data_h_l[i], data_l_h[i], data_l_l[i]},
+    {2, data_h_h[i], data_h_l[i], data_l_h[i], data_l_l[i]},
+    {3, data_h_h[i], data_h_l[i], data_l_h[i], data_l_l[i]},
+    {4, data_h_h[i], data_h_l[i], data_l_h[i], data_l_l[i]},
+  }
   got_data = static_cast<uint8_t>(serial_read_byte(pi, serial_handle));
-  if (got_data == 0x02) {
-    for (int i = 0; i < 2; ++i) {
-      byte_now = static_cast<uint8_t>(serial_read_byte(pi, serial_handle));
-      if (byte_now == 0x7D) {
-        checksum += 0x7D;
-        uint8_t next_byte =
-          static_cast<uint8_t>(serial_read_byte(pi, serial_handle));
-        byte[i] = next_byte ^ 0x20;
-        checksum += next_byte;
-      } else {
-        byte[i] = byte_now;
-        checksum += byte_now;
+  if(got_data = HEAD_BYTE){
+    got_data = static_cast<uint8_t>(serial_read_byte(pi, serial_handle));
+    if(got_data == STX){
+      checksum_receive += HEAD_BYTE;
+      checksum_receive += STX;
+      for(int k = 0; k < 5; ++k){
+        for(int i = 0; i < 5; ++i){
+          receive_data[i] = static_cast<uint8_t>(serial_read_byte(pi, serial_handle));
+          checksum_receive += receive_data[i];
+          receiveFormat[receive_data[0]][i] = receive_data[i];
+        }
       }
-    }
-    uint8_t checksum_receive =
-      static_cast<uint8_t>(serial_read_byte(pi, serial_handle));
-    if (checksum_receive == checksum) {
-      result = static_cast<uint16_t>(((byte[0] << 8) & 0xFF00) |
-          ((byte[1] << 0) & 0x00FF));
-      cout << "result : " << result << endl;
+      got_data = static_cast<uint8_t>(serial_read_byte(pi, serial_handle));
+      if(got_data == checksum_receive){
+        for(int i = 0; i < 5; ++i){
+          //receiveFormat[i][0]はidである
+          receive_result[i] = static_cast<float32_t>((receiveFormat[i][1] << 24 & 0xFF000000)
+                                           | (receiveFormat[i][2] << 16 & 0x00FF0000)
+                                           | (receiveFormat[i][3] <<  8 & 0x0000FF00)
+                                           | (receiveFormat[i][4] <<  0 & 0x000000FF)
+          );
+        }
+      }
     }
   }
 }
-void sendCallback(const std_mgs::Float64MultiArray &msg){
+void sendCallback(const std_mgs::Float32MultiArray &msg){
+  send_data[0] = msg.data[0];
+  send_data[1] = msg.data[1];
 }
 int main(int argc, char **argv) {
   ros::init(argc, argv, "stm_serial");
   ros::NodeHandle n;
-  ros::Publisher sensor_pub = n.advertise<std_msgs::Float64MultiArray>("/nucleo/sensor_info", 30);
-  ros::Subscriber serial_send_sub = n.subscribe("", 10, sendCallback);
+  ros::Publisher sensor_pub = n.advertise<std_msgs::Float32MultiArray>("/nucleo/sensor_info", 30);
+  ros::Subscriber serial_send_sub = n.subscribe("motor_speed", 10, sendCallback);
   ros::Rate loop_rate(100);
 
   int pi = pigpio_start(0, 0);
@@ -103,18 +121,14 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  uint8_t got_data{};
-  uint8_t byte_now{};
-  uint8_t byte[2]{};
-  uint16_t result{};
-  std_msgs::Float64MultiArray msg_receive;
-  msg_receive.data.resize(1);
+  std_msgs::Float32MultiArray msg_receive;
+  msg_receive.data.resize(5);
 
   thread send(sendSerial);
   thread receive(receiveSerial);
 
   while (ros::ok()) {
-    msg_receive.data[0] = result;
+    msg_receive.data[0] = receive_result;
     sensor_pub.publish(msg_receive);
     ros::spinOnce();
     loop_rate.sleep();
