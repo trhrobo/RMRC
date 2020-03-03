943ddfc64f09bd4543294902f3f0600a7327cf74
TsuruharaKota <m1830@s.akashi.ac.jp>
2020-03-03 15:43:29 +0900

    change serial using memcpy

--- comprehensive/src/stm_serial.cpp
+++ comprehensive/src/stm_serial.cpp
@@ -3,10 +3,13 @@
 #include<ros/ros.h>
 #include<std_msgs/Float32MultiArray.h>
 #include<thread>
+#include<cstdint>
+#include<cstring>
 
 using std::thread;
 using std::cout;
 using std::endl;
+using std::memcpy;
 
 #define HEAD_BYTE 0xFF
 #define STX 0x02
@@ -32,11 +35,15 @@ typedef struct{
 void sendSerial(){
   uint8_t checksum_send = 0;
   unsigned char data_h_h[2], data_h_l[2], data_l_h[2], data_l_l[2];
+  //divide byte
+  int32_t byte_divide[2];
   for(int i = 0; i < 2; ++i){
-    data_h_h[i] = (send_data[i] >> 24) & 0xFF;
-    data_h_l[i] = (send_data[i] >> 16) & 0xFF;
-    data_l_h[i] = (send_data[i] >> 8) & 0xFF;
-    data_l_l[i] = (send_data[i] >> 0) & 0xFF;
+    //float32_t to int32_t
+    memcpy(&byte_divide[i], &send_data[i], 4);
+    data_h_h[i] = (byte_divide[i] >> 24) & 0xFF;
+    data_h_l[i] = (byte_divide[i] >> 16) & 0xFF;
+    data_l_h[i] = (byte_divide[i] >> 8) & 0xFF;
+    data_l_l[i] = (byte_divide[i] >> 0) & 0xFF;
   };
   unsigned char sendFormat[2][5] = {
     {0, data_h_h[0], data_h_l[0], data_l_h[0], data_l_l[0]},
@@ -82,13 +89,15 @@ void receiveSerial(){
       }
       got_data = static_cast<uint8_t>(serial_read_byte(pi, serial_handle));
       if(got_data == checksum_receive){
+        int32_t result[5];
         for(int i = 0; i < 5; ++i){
           //receiveFormat[i][0]はidである
-          receive_result[i] = static_cast<float32_t>((receiveFormat[i][1] << 24 & 0xFF000000)
+          result[i] = static_cast<int32_t>((receiveFormat[i][1] << 24 & 0xFF000000)
                                            | (receiveFormat[i][2] << 16 & 0x00FF0000)
                                            | (receiveFormat[i][3] <<  8 & 0x0000FF00)
                                            | (receiveFormat[i][4] <<  0 & 0x000000FF)
           );
+          memcpy(&receive_result[i], &result[i], 4);
         }
       }
     }
